83a405dd24d24bb0ae83dfa47a5bd5420a479d4d
José Antonio Cuenca Espinosa <jaqnk10@gmail.com>
2019-05-11 12:47:48 +0200

    Recieve JWT from header

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
+const jwt = require('jwt-simple');
+
 const app = express();
 
 app.use(logger('dev'));
@@ -12,6 +14,11 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // const apiRoutes = require('./routes/api')
+app.use((req, res, next) => {
+    req.jwtMiddleware = false;
+
+    next();
+});
 
 app.post('/api/v1/login', (req, res, next) => {
     const [userName, password] = [req.body['user-name'], req.body.password];
@@ -25,52 +32,64 @@ app.post('/api/v1/login', (req, res, next) => {
     // Retrieve JWT for user
     // res.status(200);
     // res.json({status:200}); 
-
+req.jwtMiddleware = true;
     next();
 });
 
 app.use((req, res, next) => {
-    // if(req.jwt == null) {
-    //     res.status(401);
-    //     res.json({status:401}); 
-    //     return;            
-    // }
+    if(req.jwtMiddleware === true) {
+        // if(req.jwt == null) {
+        //     res.status(401);
+        //     res.json({status:401}); 
+        //     return;            
+        // }
+        
+        // var payload = { foo: 'bar' };
+        // var secret = 'xxx';
     
-    // console.log(req.jwt);
-    var jwt = require('jwt-simple');
-    // var payload = { foo: 'bar' };
-    // var secret = 'xxx';
- 
-    // HS256 secrets are typically 128-bit random strings, for example hex-encoded:
-    // var secret = Buffer.from('fe1a1915a379f3be5394b64d14794932', 'hex')
+        // HS256 secrets are typically 128-bit random strings, for example hex-encoded:
+        // var secret = Buffer.from('fe1a1915a379f3be5394b64d14794932', 'hex')
+        
+        // encode
+        // var token = jwt.encode(payload, secret);
     
-    // encode
-    // var token = jwt.encode(payload, secret);
- 
-    // decode
-    const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJPbmxpbmUgSldUIEJ1aWxkZXIiLCJpYXQiOjE1NTc1Njg5MjcsImV4cCI6MTU4OTEwNDkyNywiYXVkIjoid3d3LmV4YW1wbGUuY29tIiwic3ViIjoianJvY2tldEBleGFtcGxlLmNvbSIsIkdpdmVuTmFtZSI6IkpvaG5ueSIsIlN1cm5hbWUiOiJSb2NrZXQiLCJFbWFpbCI6Impyb2NrZXRAZXhhbXBsZS5jb20iLCJSb2xlIjpbIk1hbmFnZXIiLCJQcm9qZWN0IEFkbWluaXN0cmF0b3IiXX0.nIwmdlyhh21hcZiDBejXFzM0CNO7bsg4u-Uu3HWQS3s';
-    const secret = 'qwertyuiopasdfghjklzxcvbnm123456';
-    var decoded = jwt.decode(token, secret);
-    console.log(decoded); //=> { foo: 'bar' }
+        // decode
+        const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJPbmxpbmUgSldUIEJ1aWxkZXIiLCJpYXQiOjE1NTc1Njg5MjcsImV4cCI6MTU4OTEwNDkyNywiYXVkIjoid3d3LmV4YW1wbGUuY29tIiwic3ViIjoianJvY2tldEBleGFtcGxlLmNvbSIsIkdpdmVuTmFtZSI6IkpvaG5ueSIsIlN1cm5hbWUiOiJSb2NrZXQiLCJFbWFpbCI6Impyb2NrZXRAZXhhbXBsZS5jb20iLCJSb2xlIjpbIk1hbmFnZXIiLCJQcm9qZWN0IEFkbWluaXN0cmF0b3IiXX0.nIwmdlyhh21hcZiDBejXFzM0CNO7bsg4u-Uu3HWQS3s';
+        const secret = 'qwertyuiopasdfghjklzxcvbnm123456';
+        const decoded = jwt.decode(token, secret);
+        // console.log(decoded); //=> { foo: 'bar' }
 
-    req.tokenDecoded = decoded;
+        req.tokenDecoded = decoded;
 
-    /* TODO */
-    // Check if token is out of date and renew it if so.
+        /* TODO */
+        // Check if token is out of date and renew it if so.
+    }
 
     next();
 });
 
 app.use('/api/v1/', (req, res, next) => {
-    console.log(req.tokenDecoded);
+    const tokenFromBody = req.header('authorization');
 
-    next();
+    const token = tokenFromBody || req.tokenDecoded;
+
+    if(token == null) {
+        res.status(401);
+        res.json({status:401}); 
+        return;            
+    }        
+    console.log(token);
+
+    res.status(200).send();
+
+    return;
 });
 
-app.use((req, res, next) => {
-    res.status(401);
-    res.json({"status":401}); 
-    return;
-  });
+// app.use((req, res, next) => {
+//     res.status(401);
+//     res.json({"status":401}); 
+//     return;
+//   });
+
 
 module.exports = app;
