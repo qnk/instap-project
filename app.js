b3abec13e6ccc8d100fb5e07e5ac2ad5fdd3e2e9
José Antonio Cuenca Espinosa <jaqnk10@gmail.com>
2019-05-11 12:23:34 +0200

    JWT decode

--- app.js
+++ app.js
@@ -13,29 +13,64 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // const apiRoutes = require('./routes/api')
 
-app.use('/api/v1', (req, res, next) => {
-    res.status(200);
-    res.json({status:200}); 
-    return;
+app.post('/api/v1/login', (req, res, next) => {
+    const [userName, password] = [req.body['user-name'], req.body.password];
+
+    if(userName == null || password == null) {
+        res.status(401);
+        res.json({status:401}); 
+        return;        
+    }
+
+    // Retrieve JWT for user
+    // res.status(200);
+    // res.json({status:200}); 
+
+    next();
 });
 
-app.use(function(req, res, next){
+app.use((req, res, next) => {
+    // if(req.jwt == null) {
+    //     res.status(401);
+    //     res.json({status:401}); 
+    //     return;            
+    // }
+    
+    // console.log(req.jwt);
+    var jwt = require('jwt-simple');
+    // var payload = { foo: 'bar' };
+    // var secret = 'xxx';
+ 
+    // HS256 secrets are typically 128-bit random strings, for example hex-encoded:
+    // var secret = Buffer.from('fe1a1915a379f3be5394b64d14794932', 'hex')
+    
+    // encode
+    // var token = jwt.encode(payload, secret);
+ 
+    // decode
+    const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJPbmxpbmUgSldUIEJ1aWxkZXIiLCJpYXQiOjE1NTc1Njg5MjcsImV4cCI6MTU4OTEwNDkyNywiYXVkIjoid3d3LmV4YW1wbGUuY29tIiwic3ViIjoianJvY2tldEBleGFtcGxlLmNvbSIsIkdpdmVuTmFtZSI6IkpvaG5ueSIsIlN1cm5hbWUiOiJSb2NrZXQiLCJFbWFpbCI6Impyb2NrZXRAZXhhbXBsZS5jb20iLCJSb2xlIjpbIk1hbmFnZXIiLCJQcm9qZWN0IEFkbWluaXN0cmF0b3IiXX0.nIwmdlyhh21hcZiDBejXFzM0CNO7bsg4u-Uu3HWQS3s';
+    const secret = 'qwertyuiopasdfghjklzxcvbnm123456';
+    var decoded = jwt.decode(token, secret);
+    console.log(decoded); //=> { foo: 'bar' }
+
+    req.tokenDecoded = decoded;
+
+    /* TODO */
+    // Check if token is out of date and renew it if so.
+
+    next();
+});
+
+app.use('/api/v1/', (req, res, next) => {
+    console.log(req.tokenDecoded);
+
+    next();
+});
+
+app.use((req, res, next) => {
     res.status(401);
     res.json({"status":401}); 
     return;
-
-    // console.log('404');
-
-
-  
-    // // respond with json
-    // if (req.accepts('json')) {
-    //   res.send({ error: 'Not found' });
-    //   return;
-    // }
-  
-    // // default to plain-text. send()
-    // res.type('txt').send('Not found');
   });
 
 module.exports = app;
