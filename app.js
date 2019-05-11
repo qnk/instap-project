8d788d82b31ed60f1aceac7ac68b5693315576c4
José Antonio Cuenca Espinosa <jaqnk10@gmail.com>
2019-05-11 12:51:34 +0200

    Multiple paths for JWT working

--- app.js
+++ app.js
@@ -32,46 +32,14 @@ app.post('/api/v1/login', (req, res, next) => {
     // Retrieve JWT for user
     // res.status(200);
     // res.json({status:200}); 
-req.jwtMiddleware = true;
-    next();
-});
-
-app.use((req, res, next) => {
-    if(req.jwtMiddleware === true) {
-        // if(req.jwt == null) {
-        //     res.status(401);
-        //     res.json({status:401}); 
-        //     return;            
-        // }
-        
-        // var payload = { foo: 'bar' };
-        // var secret = 'xxx';
-    
-        // HS256 secrets are typically 128-bit random strings, for example hex-encoded:
-        // var secret = Buffer.from('fe1a1915a379f3be5394b64d14794932', 'hex')
-        
-        // encode
-        // var token = jwt.encode(payload, secret);
-    
-        // decode
-        const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJPbmxpbmUgSldUIEJ1aWxkZXIiLCJpYXQiOjE1NTc1Njg5MjcsImV4cCI6MTU4OTEwNDkyNywiYXVkIjoid3d3LmV4YW1wbGUuY29tIiwic3ViIjoianJvY2tldEBleGFtcGxlLmNvbSIsIkdpdmVuTmFtZSI6IkpvaG5ueSIsIlN1cm5hbWUiOiJSb2NrZXQiLCJFbWFpbCI6Impyb2NrZXRAZXhhbXBsZS5jb20iLCJSb2xlIjpbIk1hbmFnZXIiLCJQcm9qZWN0IEFkbWluaXN0cmF0b3IiXX0.nIwmdlyhh21hcZiDBejXFzM0CNO7bsg4u-Uu3HWQS3s';
-        const secret = 'qwertyuiopasdfghjklzxcvbnm123456';
-        const decoded = jwt.decode(token, secret);
-        // console.log(decoded); //=> { foo: 'bar' }
-
-        req.tokenDecoded = decoded;
-
-        /* TODO */
-        // Check if token is out of date and renew it if so.
-    }
-
+req.tokenFromLogin = true;
     next();
 });
 
 app.use('/api/v1/', (req, res, next) => {
     const tokenFromBody = req.header('authorization');
 
-    const token = tokenFromBody || req.tokenDecoded;
+    const token = tokenFromBody || req.tokenFromLogin;
 
     if(token == null) {
         res.status(401);
@@ -80,7 +48,14 @@ app.use('/api/v1/', (req, res, next) => {
     }        
     console.log(token);
 
-    res.status(200).send();
+    const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJPbmxpbmUgSldUIEJ1aWxkZXIiLCJpYXQiOjE1NTc1Njg5MjcsImV4cCI6MTU4OTEwNDkyNywiYXVkIjoid3d3LmV4YW1wbGUuY29tIiwic3ViIjoianJvY2tldEBleGFtcGxlLmNvbSIsIkdpdmVuTmFtZSI6IkpvaG5ueSIsIlN1cm5hbWUiOiJSb2NrZXQiLCJFbWFpbCI6Impyb2NrZXRAZXhhbXBsZS5jb20iLCJSb2xlIjpbIk1hbmFnZXIiLCJQcm9qZWN0IEFkbWluaXN0cmF0b3IiXX0.nIwmdlyhh21hcZiDBejXFzM0CNO7bsg4u-Uu3HWQS3s';
+    const secret = 'qwertyuiopasdfghjklzxcvbnm123456';
+    const decoded = jwt.decode(token, secret);
+
+    /* TODO */
+    // Check if token is out of date and renew it if so.
+
+    res.status(200).send({'jwt': decoded});
 
     return;
 });
